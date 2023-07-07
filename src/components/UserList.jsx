d96594685d90e345357aaf3df8a485aa5a4716f8
ANSHUL VERMA <anshul@ANSHULs-Air.realtek>
2023-07-07 12:09:00 +0800

    title change

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -42,7 +42,7 @@ export default function UserList() {
   return (
     <>
       <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-        User
+        Users
       </h1>
 
       <div>
